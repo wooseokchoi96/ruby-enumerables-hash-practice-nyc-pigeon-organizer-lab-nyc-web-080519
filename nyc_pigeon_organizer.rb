d116217b43a0c566221c7458f9e58687e450d8f2
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 20:05:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(data)
     }
   }
   pigeon_names.each{ |piggy_name|
-    pigeon_list[piggy_name] = Hash.new
+    pigeon_list[piggy_name] = Hash.new{ |a, b| a[b] = [] }
     data.each{ |attribute, value|
       
     }

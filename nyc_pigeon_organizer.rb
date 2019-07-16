b528891ce7a80b732378b6c7c925463052534106
Andy Choi <wooseokchoi96@gmail.com>
2019-07-16 19:45:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each{ |attribute,value|
     value.each{ |name|
-      if !pigeon_list[name]
+      if !pigeon_list[name].nil?
         pigeon_list[name]: {}
       end
     }
